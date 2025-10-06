7f0de48aa56a73293df71f9897e97d1bb14e0163
sahal <sahal.g3interactive@gmail.com>
2025-10-06 15:54:45 +0530

    First commit

--- eclipse-workspace/Itype4Home/src/test/java/login_pages_test/ForgotPassword_Test.java
+++ eclipse-workspace/Itype4Home/src/test/java/login_pages_test/ForgotPassword_Test.java
@@ -2,9 +2,7 @@ package login_pages_test;
 
 import java.io.IOException;
 import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import base_packages.Initial_step;
 
@@ -12,7 +10,7 @@ public class ForgotPassword_Test extends Initial_step {
 	
 	Login_Page_Elements LPE;
 	String Mail="anto.g3interactive+superadmin@gmail.com";
-	String Password="";
+	WebDriverWait wait;
 	
 	
 	@Test (priority = 1)
@@ -75,7 +73,7 @@ public class ForgotPassword_Test extends Initial_step {
 		
 	}
 	
-	@Test
+	@Test (priority = 3)
 	public void click_backto_signin_button() throws IOException, InterruptedException
 	{
 		LPE= new Login_Page_Elements(driver, testcase);
@@ -83,22 +81,85 @@ public class ForgotPassword_Test extends Initial_step {
 		Thread.sleep(1000);
 		
 		testcase= extendReport.createTest("Verify forgotpassword - back to signin button is working");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
-		LPE.Click_forgot_password();
-		LPE.Click_forgotpassword_backtoSignin_button();
 		Thread.sleep(2000);
+		LPE.Click_forgot_password();
+		Thread.sleep(2000);
+		LPE.Click_forgotpassword_backtoSignin_button();
 		
 		if(LPE.Login_page_Title_Verification().isDisplayed())
 		{
 			
-			testcase.log(PASS, "back to sign in button is working as expected");
+			testcase.log(PASS, "Back To Signin Button is working as expected");
 			Thread.sleep(2000);
-			takescreenshot(driver, "button is working properly" );
+			takescreenshot(driver, "Back To Signin Nutton is working as expected" );
 		}
 		else
 		{
-			testcase.log(FAIL, "continue button is not working as expected");
+			testcase.log(FAIL, "Back To Signin Button is not working");
+			Thread.sleep(2000);
+			takescreenshot(driver, "failed");
+		}
+		
+	}
+	
+	@Test (priority = 4)
+	public void Verify_forgot_password_email_validation_message() throws IOException, InterruptedException
+	{
+		LPE= new Login_Page_Elements(driver, testcase);
+		Logout();
+		Thread.sleep(1000);
+		
+		testcase= extendReport.createTest("Verify validation messege is apearing in emailid feild without enter emailid");
+		
+		Thread.sleep(2000);
+		LPE.Click_forgot_password();
+		Thread.sleep(2000);
+		LPE.Click_forgotpassword_continue_button();
+		
+		if(LPE.Verify_validationMessage_Email().isDisplayed())
+		{
+			
+			testcase.log(PASS, "Validation messege is displayed succsfully");
+			Thread.sleep(2000);
+			takescreenshot(driver, "Back To Signin Nutton is working as expected" );
+		}
+		else
+		{
+			testcase.log(FAIL, "Validation messege is not displayed");
+			Thread.sleep(2000);
+			takescreenshot(driver, "failed");
+		}
+	}
+	
+	@Test (priority = 5)
+	public void Verify_Resetpassword_link_is_sent() throws IOException, InterruptedException
+	{
+		LPE= new Login_Page_Elements(driver, testcase);
+		Logout();
+		Thread.sleep(1000);
+		
+		testcase= extendReport.createTest("Verify reset password link is sent succesfully");
+		
+		Thread.sleep(2000);
+		LPE.Click_forgot_password();
+		LPE.Enter_forgotpassword_emailID(Mail);
+		LPE.Click_forgotpassword_continue_button();
+		Thread.sleep(2000);
+		
+		wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		Thread.sleep(2000);
+	
+		if(LPE.Verify_Reset_Password_Link().isDisplayed())
+		{
+			takescreenshot(driver, "The Success Messege is displayed Succesfuly" );
+			testcase.log(PASS, "The Success Messege is displayed Succesfuly");
+			Thread.sleep(2000);
+			
+		}
+		else
+		{
+			testcase.log(FAIL, "messege is not displayed");
 			Thread.sleep(2000);
 			takescreenshot(driver, "failed");
 		}
@@ -106,7 +167,9 @@ public class ForgotPassword_Test extends Initial_step {
 		
 	
 
-}
+	}
+	
+	
 	
 	
 	}
